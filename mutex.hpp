3759aa1547e0843616c88eb969ba02f87c042332
Breeze Chen <breezechen356@gmail.com>
2017-04-16 04:20:59 +0800

    修复linux下编译错误

--- mutex.hpp
+++ mutex.hpp
@@ -1,20 +1,33 @@
 #ifndef _BR_MUTEX_LOCKER_H_
 #define _BR_MUTEX_LOCKER_H_
-#include <windows.h>
+
 
 namespace br {
 #if defined(__GNUC__) || defined(__GNUG__)
+#include <time.h>
 #define __br_compare_and_swap(x, o, n) __sync_val_compare_and_swap(x, o, n)
 #define __br_fetch_and_add(x, v) __sync_fetch_and_add(x, v)
 #define __br_fetch_and_sub(x, v) __sync_fetch_and_sub(x, v)
 #elif defined(_MSC_VER)
+#include <windows.h>
 #define __br_compare_and_swap(x, o, n) InterlockedCompareExchange(x, n, o)   
 #define __br_fetch_and_add(x, v) InterlockedExchangeAdd(x, v)
 #define __br_fetch_and_sub(x, v) InterlockedExchangeAdd(x, -v) 
 #endif
+
+    void msleep(int ms)
+    {
+#ifdef WIN32
+        Sleep(ms);
+#else
+        struct timespec ts = { ms / 1000, (ms % 1000) * 1000 * 1000 };
+        nanosleep(&ts, NULL);
+#endif
+    }
+
     struct mutex
     {
-        volatile LONG sem;
+        volatile long sem;
         volatile bool askwrite;
         mutex() :sem(0), askwrite(false) {}
     };
@@ -26,16 +39,16 @@ namespace br {
             if (_write) { // big brother
                 while (__br_compare_and_swap(&_m.sem, 0, -1) != 0) { //someone taking my toy
                     _m.askwrite = true; // I need the toy right now!!!
-                    Sleep(1); // wait
+                    msleep(1); // wait
                 }
                 _m.askwrite = false; // your buddies can competing for it now
             } else {
                 while(1) {
                     if (_m.askwrite) { // the big brother is looking for it, stand by
-                        Sleep(1);
+                        msleep(1);
                     } else if (__br_fetch_and_add(&_m.sem, 2) <= -1) { // big brother is enjoying
                         __br_fetch_and_sub(&_m.sem, 2); // sorry, I'm not mean it, step back
-                        Sleep(1);
+                        msleep(1);
                     } else { // we can share it now
                         break;
                     }
